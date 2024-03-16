c752e0269fad73fe8819f46977c6188b95f05f5d
rijadhmz <rijadhamzic2000@gmail.com>
2024-03-16 15:04:35 +0100

    removed insert many data entries from index to seeds.js

--- seeds.js
+++ seeds.js
@@ -13,31 +13,78 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmShop')
     
 const seedProducts = [
     {
-        name: 'Apple',
-        price: 0.99,
-        category: 'fruit'
+      name: 'Apple',
+      price: 0.99,
+      category: 'fruit',
+      __v: 0
     },
     {
-        name: 'Milk',
-        price: 1.99,
-        category: 'dairy'
+      name: 'Carrot',
+      price: 0.49,
+      category: 'vegetable',
+      __v: 0
     },
     {
-        name: 'Cheese',
-        price: 2.99,
-        category: 'dairy'
+      name: 'Potato',
+      price: 0.99,
+      category: 'vegetable',
+      __v: 0
     },
     {
-        name: 'Carrot',
-        price: 0.49,
-        category: 'vegetable'
+      name: 'Milk',
+      price: 1.99,
+      category: 'dairy',
+      __v: 0
     },
     {
-        name: 'Potato',
-        price: 0.99,
-        category: 'vegetable'
+      name: 'Cheese',
+      price: 2.99,
+      category: 'dairy',
+      __v: 0
+    },
+    {
+      name: 'Strawberry',
+      price: 0.19,
+      category: 'fruit',
+      __v: 0
+    },
+    {
+      name: 'Plum',
+      price: 0.39,
+      category: 'fruit',
+      __v: 0
+    },
+    {
+      name: 'Pear',
+      price: 0.79,
+      category: 'fruit',
+      __v: 0
+    },
+    {
+      name: 'Tomato',
+      price: 0.99,
+      category: 'vegetable',
+      __v: 0
+    },
+    {
+      name: 'Onion',
+      price: 0.49,
+      category: 'vegetable',
+      __v: 0
+    },
+    {
+      name: 'Butter',
+      price: 4.99,
+      category: 'dairy',
+      __v: 0
+    },
+    {
+      name: 'Yoghurt',
+      price: 2.99,
+      category: 'dairy',
+      __v: 0
     }
-]
+];
 
 Product.insertMany(seedProducts)
 .then(res => {
